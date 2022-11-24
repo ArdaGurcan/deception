18fa8bcdfc87a34af8d59aeb6bc28508f05d97ea
ArdaGurcan <ardagurcan2004@gmail.com>
2022-11-23 22:45:42 -0600

    add gitignore

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  Transform mainCamera;
+
+  void Start()
+  {
+    mainCamera = Camera.main.transform;
+    transform.Rotate(90, 0, 0);
+  }
+
+  void Update()
+  {
+    if (Vector3.SqrMagnitude(mainCamera.position - transform.position) > 10000)
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (!other.CompareTag("Player"))
+      Destroy(gameObject);
+  }
+}
---
+++ Assets/Shooter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bulletPrefab;
+
+    [SerializeField]
+    Transform mainCamera;
+
+    [SerializeField]
+    float bulletSpeed = 5f;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if(Mouse.current.leftButton.wasPressedThisFrame) {
+            GameObject bullet = Instantiate(bulletPrefab, mainCamera.position, mainCamera.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = mainCamera.forward * bulletSpeed;
+        }
+    }
+}
