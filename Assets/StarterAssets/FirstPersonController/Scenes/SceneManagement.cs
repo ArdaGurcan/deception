ad912567da14dd1e6f41db452e4fdfa41d6dc46d
BreakfastNoo <111155645+BreakfastNoo@users.noreply.github.com>
2022-11-26 21:53:53 -0600

    revert

--- Assets/StarterAssets/FirstPersonController/Scenes/SceneManagement.cs
+++ Assets/StarterAssets/FirstPersonController/Scenes/SceneManagement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
-public class SceneManagement : MonoBehaviour {
+public class SceneManagement : MonoBehaviour
+{
 
     // Start is called before the first frame update
-    void Start() {
-        SceneManager.LoadScene(0);
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
