a7b75d581841685377e963a6e97524cfb20d8b87
ArdaGurcan <ardagurcan2004@gmail.com>
2022-11-27 21:26:32 -0600

    fix crosshair

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -6,30 +6,55 @@ using UnityEngine.InputSystem;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField]
-    GameObject bulletPrefab;
+  [SerializeField]
+  GameObject bulletPrefab;
 
-    [SerializeField]
-    Transform mainCamera;
+  [SerializeField]
+  Camera mainCamera;
 
-    [SerializeField]
-    float bulletSpeed = 10f;
-
+  [SerializeField]
+  float bulletSpeed = 10f;
+  [SerializeField]
+  Transform guntip;
+  [SerializeField]
+  Transform gun;
   // Start is called before the first frame update
   void Start()
-    {
-        mainCamera = Camera.main.transform;
-    }
+  {
+    mainCamera = Camera.main;
+  }
 
-    // Update is called once per frame
-    void Update()
-    {  
-        if(Mouse.current.leftButton.wasPressedThisFrame) {
-            // var x = mainCamera.transform.localEulerAngles.x * Mathf.PI / 180f;
-           // var z = mainCamera.transform.localEulerAngles.x * Mathf.PI / 180f;
-            GameObject bullet = Instantiate(bulletPrefab, mainCamera.position + mainCamera.right*0.5f + mainCamera.up*-0.2f, mainCamera.rotation);//new Vector3(mainCamera.position.x + 0.5f * Mathf.Sin(x), mainCamera.position.y - 0.5f, mainCamera.position.z + 0.5f * Mathf.Sin(z)) , mainCamera.rotation);
-            
-            bullet.GetComponent<Rigidbody>().velocity = mainCamera.forward * bulletSpeed;
-        }
+  void OnDrawGizmos()
+  {
+    Vector3 screenCenter = Camera.main.ScreenToViewportPoint(new Vector3(.5f, .5f, 0));
+    Gizmos.DrawSphere(screenCenter, 0.1f);
+    Ray ray = new Ray(guntip.position, screenCenter);
+    Gizmos.DrawLine(guntip.position, guntip.position + ray.direction);
+  }
+  // Update is called once per frame
+  void Update()
+  {
+
+    if (Mouse.current.leftButton.wasPressedThisFrame)
+    {
+      RaycastHit hit;
+      GameObject bullet = Instantiate(bulletPrefab, guntip.position, Quaternion.identity);//new Vector3(mainCamera.position.x + 0.5f * Mathf.Sin(x), mainCamera.position.y - 0.5f, mainCamera.position.z + 0.5f * Mathf.Sin(z)) , mainCamera.rotation);
+      if(Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, 100)) {
+          bullet.transform.LookAt(hit.point);
+      } else {
+        bullet.transform.rotation = mainCamera.transform.rotation;
+      }
+
+
+
+      bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
     }
+    //   // var x = mainCamera.transform.localEulerAngles.x * Mathf.PI / 180f;
+    //   // var z = mainCamera.transform.localEulerAngles.x * Mathf.PI / 180f;
+    //   Vector3 screenCenter = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width/2, Screen.height/2, 0));
+    //   Ray ray = new Ray(guntip.position, screenCenter);
+    //   GameObject bullet = Instantiate(bulletPrefab, guntip.position, Quaternion.Euler(ray.direction));//new Vector3(mainCamera.position.x + 0.5f * Mathf.Sin(x), mainCamera.position.y - 0.5f, mainCamera.position.z + 0.5f * Mathf.Sin(z)) , mainCamera.rotation);
+
+  }
 }
+
